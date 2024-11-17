5d0e2d7e88e9f114c0c4a427425e7d62084246b6
SophaktraLeng <sopheaktraleng4@gmail.com>
2024-11-17 18:36:16 +0700

    git ignore

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 // const express = require('express')
 import express from "express"
 const app = express();
+app.get("/",(req,res)=>{
+    res.send('Server is running')
+})
 app.listen(5001, ()=>{
-    console.log("Server is running on http://localhost:5000")
+    console.log("Server is running on http://localhost:5001")
 })
