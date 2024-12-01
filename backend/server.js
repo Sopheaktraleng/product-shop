e67e065e66d9398abcea16f49739520e64777be5
Leng Sopheaktra <sopheaktraleng4@gmail.com>
2024-12-01 22:38:53 +0700

    config-db

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ const app = express();
 app.get("/",(req,res)=>{
     res.send('Server is running')
 })
+app.get("/home",(res,req)=>{
+    res.send('hello')
+})
 app.listen(5001, ()=>{
     console.log("Server is running on http://localhost:5001")
 })
